53e31c9d935482d1133fa0aae40acc82052f9a2b
Xhh9850 <xhh9850@g.rit.edu>
2021-11-03 23:45:56 -0400

    Content
    Everything

---
+++ Artifacts.php
@@ -0,0 +1,19 @@
+
+<body>
+    <div class="container">
+        <nav class="menu">
+            <ul>
+                <li><a href="index.html"> Main Page </a></li>
+                <li><a href="Starting.php"> Starting </a></li>
+                <li><a href="Characters.php"> Characters </a></li>
+                <li><a href="Weapons.php"> Weapons </a></li>
+                <li><a class= "is-current" href="Artifacts.php"> Artifacts </a></li>
+                <li><a href="Resources.php"> Resources </a></li>
+                <li><a href="Domains.php"> Domains </a></li>
+                <li><a href="Weekly_Bosses.php"> Weekly_Bosses </a></li>
+            </ul>
+        </nav>
+        <?php include "inc/html.php"; ?>
+    </div>
+    <!--.container-->
+</body>
---
+++ Weekly_Bosses.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1- Genshin Impact</title>
+    <link rel="stylesheet" href="css/Weekly_Bosses.css">
+    <link rel="stylesheet" href="css/Navigation.css">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="menu">
+            <ul>
+                <li><a href="index.html"> Main Page </a></li>
+                <li><a href="Starting.php"> Starting </a></li>
+                <li><a href="Characters.php"> Characters </a></li>
+                <li><a href="Weapons.php"> Weapons </a></li>
+                <li><a href="Artifacts.php"> Artifacts </a></li>
+                <li><a href="Resources.php"> Resources </a></li>
+                <li><a href="Domains.php"> Domains </a></li>
+                <li><a class = "is-current" href="Weekly_Bosses.php"> Weekly_Bosses </a></li>
+            </ul>
+        </nav>
+        <?php include "inc/html.php"; ?>
+    </div>
+    <!--.container-->
+</body>
