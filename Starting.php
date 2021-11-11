3a08b754cf7e3210998e563c8ceb821ff9d959a2
GuyGriner <guygriner@yahoo.com>
2021-11-10 19:23:47 -0500

    made text edits
    Made a mistake and put the text into wrong page

--- Starting.php
+++ Starting.php
@@ -28,7 +28,7 @@
     </div>
 	<h1> "Getting started" </h1>
 	<p>
-	Genshin Impact is a popular video game available on mobile personal computers which has a heavy focus on exploration combat. Genshin has a multitude of colorful characters, weapons, and upgrades that players can spend hours in trying to get what they need to progress. With the game’s extreme player-driven focus makes the seemingly simple game has a very complex core that newer players may struggle to understand. This can lead to newer players missing out on one of gaming's most powerful phenomenons of the past few years. This site will attempt to alleviate some of the troubles and questions that new players may have so that they can get straight to enjoying this breathtaking and enjoyable game to their fullest extent.
+	
 	</p>
     <!--.container-->
 </body>
